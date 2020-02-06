34383e2651d5ea65ed3275b035e83872cd8d75e5
dfutida <d.futida@hotmail.com>
2020-02-05 23:54:20 -0300

    update file

--- CRUD/index.js
+++ CRUD/index.js
@@ -4,7 +4,7 @@ const app = express()
 const porta = 3002
 
 const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb+srv://denis:LXKBPJRHuEcIpL9r@cluster0-x8op1.mongodb.net/test?retryWrites=true&w=majority"
+const uri = "mongodb+srv://<user>:<password>@cluster0-x8op1.mongodb.net/test?retryWrites=true&w=majority"
 
 const ObjectId = require('mongodb').ObjectID
 //const cliente = new MongoClient(uri, { useUnifiedTopology: true });
